c973a977a60b994c1f9c1aaf5eaa6a2c977c7e7a
JorgeWS <jorgews.dev@gmail.com>
2025-05-04 19:46:36 -0300

    feat: 🎸 Criará o doc do botão

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@abqm-ui/react'
+import { ArrowRight } from 'phosphor-react'
 
 export default {
   title: 'Form/Button',
@@ -11,8 +12,42 @@ export default {
 
 export const Primary: StoryObj<ButtonProps> = {}
 
-export const Big: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
-    size: 'big',
+    variant: 'secondary',
+    children: 'Criar',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancelar',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: 'Enviar',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: (
+      <>
+        Próximo Passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Desabilitado',
+    disabled: true,
   },
 }
