579ad3f75ef2755cf3f84af2d2439fb987cb408e
JorgeWS <jorgews.dev@gmail.com>
2025-05-04 22:33:06 -0300

    fix: 🐛 Alterará importações de libs do storybook e setará o tema dark no arquivo manager.ts

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -6,36 +6,34 @@ import { join, dirname } from "path"
 * This function is used to resolve the absolute path of a package.
 * It is needed in projects that use Yarn PnP or are set up within a monorepo.
 */
-// function getAbsolutePath(value: string): any {
-//   return dirname(require.resolve(join(value, 'package.json')))
-// }
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
 const config: StorybookConfig = {
   "stories": [
     "../src/pages/**/*.mdx",
     "../src/stories/**/*.stories.tsx"
   ],
-  "addons": [
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
-    "@storybook/experimental-addon-test"
-  ],
   // "addons": [
-  //   getAbsolutePath('@storybook/addon-essentials'),
-  //   getAbsolutePath('@storybook/addon-onboarding'),
-  //   getAbsolutePath('@storybook/addon-docs'),
-  //   getAbsolutePath('@storybook/addon-links'),
-  //   getAbsolutePath('@chromatic-com/storybook'),
-  //   getAbsolutePath("@storybook/experimental-addon-test")
+  //   '@storybook/addon-essentials',
+  //   '@storybook/addon-onboarding',
+  //   '@chromatic-com/storybook',
+  //   "@storybook/experimental-addon-test"
   // ],
-  "framework": {
-    "name": '@storybook/react-vite',
-    "options": {}
-  },
+  "addons": [
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath("@storybook/experimental-addon-test")
+  ],
   // "framework": {
-  //   "name": getAbsolutePath('@storybook/react-vite'),
+  //   "name": '@storybook/react-vite',
   //   "options": {}
   // },
+  "framework": {
+    "name": getAbsolutePath('@storybook/react-vite'),
+    "options": {}
+  },
   docs: {
     autodocs: true, // Or true, enables autodocs for non-MDX stories
   },
--- packages/docs/.storybook/manager.ts
+++ packages/docs/.storybook/manager.ts
@@ -1,9 +1,10 @@
 // .storybook/manager.ts
 import { addons } from '@storybook/manager-api'
 import { create } from '@storybook/theming/create'
+import theme from './theme'
 
 const customTheme = create({
-  base: 'light',
+  base: theme,
   brandTitle: 'ABQM Design System',
   brandUrl: 'https://abqm.com.br',
   // brandImage: 'https://abqm.com.br/logo.png', // Opcional
