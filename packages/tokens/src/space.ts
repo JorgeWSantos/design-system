ee546aa2cd4e14767d27cd54b47bee70b7cb4913
JorgeWS <jorgews.dev@gmail.com>
2025-05-04 23:05:14 -0300

    styles: 🌈 Adicionará comentário relevante ao token de space

--- packages/tokens/src/space.ts
+++ packages/tokens/src/space.ts
@@ -1,8 +1,8 @@
 export const space = {
-  1: '0.25rem',
-  2: '0.5rem',
+  1: '0.25rem', // 1 x 4 é igual a 4, e 4px são 0.25rem
+  2: '0.5rem', // 2 x 4 é igual a 8, e 8px são 0.5rem
   3: '0.75rem',
-  4: '1rem',
+  4: '1rem', // 4 x 4 é igual a 16, e 16px equivale a 1rem
   5: '1.25rem',
   6: '1.5rem',
   7: '1.75rem',
