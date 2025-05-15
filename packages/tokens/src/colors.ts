d6a635e05358346aa4138598774a9b079e4e673b
JorgeWS <jorgews.dev@gmail.com>
2025-05-14 21:53:33 -0300

    feat: 🎸 Adicionará novas cores ao projeto

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -2,6 +2,7 @@ export const colors = {
   white: '#FFF',
   black: '#000',
 
+  // grays
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
   gray400: '#7C7C8A',
@@ -9,12 +10,38 @@ export const colors = {
   gray600: '#323238',
   gray700: '#29292E',
   gray800: '#202024',
+  gray850: '#1B1B1B',
   gray900: '#121214',
+
+  // opacitys
+  white25: '#ffffff40',
+  white50: '#ffffff80',
+  white85: '#ffffffd9',
+
+  black25: '#00000040',
+  black50: '#00000080',
+  black75: '#000000BF',
+  black85: '#000000d9',
+
+  emeraldGreen50: '#00100780',
+  emeraldGreen75: '#001007BF',
+
+  brown500: '#A65E2E',
+  brown700: '#5A2412',
+
+  yellow200: '#FFE596',
+  yellow400: '#FFCD00',
+
+  green100: '#95C23D',
+  green300: '#009843',
+  green500: '#006B2D',
+  green700: '#476153',
+  green900: '#002F14',
+
+  blue900: '#003770',
 
   ignite300: '#00B37E',
   ignite500: '#00875F',
   ignite700: '#015F43',
   ignite900: '#00291D',
-
-  test: '#00291D',
 }
