9b5a17b545f895136471f128232e91104fb78c4d
JorgeWS <jorgews.dev@gmail.com>
2025-05-17 20:19:42 -0300

    feat: 🎸 Adicionará melhorias no componente heading

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -1,11 +1,12 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Heading, HeadingProps } from '@abqm-ui2/react'
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading, HeadingProps } from '@abqm-ui2/react';
 
 export default {
   title: 'Typography/Heading',
   component: Heading,
   args: {
     size: 'md',
+    weight: 'semi',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit vero esse placeat quae et blanditiis, aspernatur ut itaque possimus ea autem alias ex minima tenetur reiciendis natus ipsa eligendi! Soluta.',
   },
@@ -14,10 +15,14 @@ export default {
       options: ['sm', 'md', 'lg', '2xl', '3xl', '4xl', '5xl', '6xl'],
       control: { type: 'inline-radio' },
     },
+    weight: {
+      options: ['regular', 'medium', 'semi', 'bold'],
+      control: { type: 'inline-radio' },
+    },
   },
-} as Meta<HeadingProps>
+} as Meta<HeadingProps>;
 
-export const Primary: StoryObj<HeadingProps> = {}
+export const Primary: StoryObj<HeadingProps> = {};
 
 export const CustomTag: StoryObj<HeadingProps> = {
   args: {
@@ -32,4 +37,4 @@ export const CustomTag: StoryObj<HeadingProps> = {
       },
     },
   },
-}
+};
--- packages/react/src/components/Heading.tsx
+++ packages/react/src/components/Heading.tsx
@@ -1,7 +1,7 @@
 // Heading.tsx
-import styled, { css } from 'styled-components'
-import { ComponentProps, ElementType, ReactNode } from 'react'
-import { theme } from '../styles'
+import styled, { css } from 'styled-components';
+import { ComponentProps, ElementType, ReactNode } from 'react';
+import { theme } from '../styles';
 
 const sizeVariants = {
   sm: theme.fontSizes.xl,
@@ -12,12 +12,21 @@ const sizeVariants = {
   '4xl': theme.fontSizes['7xl'],
   '5xl': theme.fontSizes['8xl'],
   '6xl': theme.fontSizes['9xl'],
-}
+};
 
-type SizeVariants = keyof typeof sizeVariants
+const weightVariants = {
+  regular: theme.fontWeights.regular,
+  medium: theme.fontWeights.medium,
+  semi: theme.fontWeights.semiBold,
+  bold: theme.fontWeights.bold,
+};
+
+type SizeVariants = keyof typeof sizeVariants;
+type WeightVariants = keyof typeof weightVariants;
 
 interface StyledHeadingProps {
-  size?: SizeVariants
+  size?: SizeVariants;
+  weight?: WeightVariants;
 }
 
 const StyledHeading = styled.h2<StyledHeadingProps>`
@@ -32,20 +41,33 @@ const StyledHeading = styled.h2<StyledHeadingProps>`
     css`
       font-size: ${sizeVariants[size]};
     `}
-`
+
+  ${({ weight }) =>
+    weight &&
+    css`
+      font-weight: ${weightVariants[weight]};
+    `}
+`;
 
 export interface HeadingProps extends ComponentProps<typeof StyledHeading> {
-  as?: ElementType
-  children: ReactNode
-  size?: SizeVariants
+  as?: ElementType;
+  children: ReactNode;
+  size?: SizeVariants;
+  weight?: WeightVariants;
 }
 
-export function Heading({ children, size = 'md', as = 'h2', ...rest }: HeadingProps) {
+export function Heading({
+  children,
+  size = 'md',
+  as = 'h2',
+  weight,
+  ...rest
+}: HeadingProps) {
   return (
-    <StyledHeading as={as} size={size} {...rest}>
+    <StyledHeading as={as} size={size} weight={weight} {...rest}>
       {children}
     </StyledHeading>
-  )
+  );
 }
 
-Heading.displayName = 'Heading'
+Heading.displayName = 'Heading';
