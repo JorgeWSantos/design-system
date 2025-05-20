676b3d7260c80804f38ecc4523f33634ffabd793
JorgeWS <jorgews.dev@gmail.com>
2025-05-20 19:59:45 -0300

    feat: 🎸 Adicionará arquivo para corrigir erro de importação

---
+++ packages/react/src/types/@abqm-ui2__icons.d.ts
@@ -0,0 +1,7 @@
+// declare module '@abqm-ui2/icons' {
+//   // import { FC, SVGProps } from 'react';
+//   // export const FilterIcon: FC<SVGProps<SVGSVGElement>>;
+//   // export const PlusIcon: FC<SVGProps<SVGSVGElement>>;
+//   // export const PrinterIcon: FC<SVGProps<SVGSVGElement>>;
+//   // Adicione outros ícones exportados aqui
+// }
