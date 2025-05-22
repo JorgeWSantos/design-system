a3e50145dc2481991c3638eb22e708e62a253684
JorgeWS <jorgews.dev@gmail.com>
2025-05-21 23:42:33 -0300

    feat: 🎸 Adicionará novo token

---
+++ packages/tokens/src/breakpoints.ts
@@ -0,0 +1,15 @@
+export const breakpoints = {
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1400,
+};
+
+export const breakpointsPx = {
+  sm: breakpoints.sm + 'px',
+  md: breakpoints.md + 'px',
+  lg: breakpoints.lg + 'px',
+  xl: breakpoints.xl + 'px',
+  xxl: breakpoints.xxl + 'px',
+};
