96427f85a388b0ae8aeb80598e792287706463bb
JorgeWS <jorgews.dev@gmail.com>
2025-05-22 00:21:46 -0300

    feat: 🎸 Adicionará novos spaces

--- packages/tokens/src/space.ts
+++ packages/tokens/src/space.ts
@@ -1,18 +1,21 @@
 export const space = {
-  1: '0.25rem', // 1 x 4 é igual a 4, e 4px são 0.25rem
+  1: '0.25rem', // 1 x 4 = 4, e 4px são 0.25rem
   1.5: '0.375rem',
-  2: '0.5rem', // 2 x 4 é igual a 8, e 8px são 0.5rem
+  2: '0.5rem', // 2 x 4 = 8, e 8px são 0.5rem
+  2.5: '0.625rem',
   3: '0.75rem',
-  4: '1rem', // 4 x 4 é igual a 16, e 16px equivale a 1rem
+  4: '1rem', // 4 x 4 = 16, e 16px equivale a 1rem
   5: '1.25rem',
   6: '1.5rem',
+  6.5: '1.625rem',
   7: '1.75rem',
   8: '2rem',
   10: '2.5rem',
   12: '3rem',
   16: '4rem',
   20: '5rem',
+  26: '6.5rem', // 26 x 4 = 104px
   40: '10rem',
   64: '16rem',
-  80: '20rem'
-}
+  80: '20rem',
+};
