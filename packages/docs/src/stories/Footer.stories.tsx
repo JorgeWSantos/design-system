4ce16f26e3421a1c795a962c218825682f6a5f05
JorgeWS <jorgews.dev@gmail.com>
2025-05-22 00:24:03 -0300

    fix: 🐛 Fará correções necessário no Component Footer

--- packages/docs/src/stories/Footer.stories.tsx
+++ packages/docs/src/stories/Footer.stories.tsx
@@ -1,25 +1,9 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Footer, FooterProps } from '@abqm-ui2/react'
+import { Meta, StoryObj } from '@storybook/react';
+import { Footer, FooterProps } from '@abqm-ui2/react';
 
 export default {
   title: 'Surfaces/Footer',
   component: Footer,
-} as Meta<FooterProps>
+} as Meta<FooterProps>;
 
-export const Primary: StoryObj<FooterProps> = {
-}
-
-export const Secondary: StoryObj<FooterProps> = {
-  args: {
-    // style: { backgroundColor: 'green', border: '1px solid red' } //example
-    size: 'md'
-  }
-}
-
-export const Tertiary: StoryObj<FooterProps> = {
-  args: {
-    // style: { backgroundColor: 'green', border: '1px solid red' } //example
-    showIcons: false,
-    size: 'sm'
-  }
-}
+export const Primary: StoryObj<FooterProps> = {};
