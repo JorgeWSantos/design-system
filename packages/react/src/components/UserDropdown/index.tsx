df24c82f9dd3d5e2ed0c5da2476cffff1e9205d4
JorgeWS <jorgews.dev@gmail.com>
2025-05-24 20:04:07 -0300

    feat: 🎸 Permitirá o componente UserDropdown receber imagem externa

--- packages/react/src/components/UserDropdown/index.tsx
+++ packages/react/src/components/UserDropdown/index.tsx
@@ -6,15 +6,17 @@ import { CaretDownIcon } from '@abqm-ui2/icons';
 export interface UserDropDownProps extends ComponentProps<typeof UserDropdownWrapper> {
   as?: ElementType;
   userName: string;
+  srcImage: string;
 }
 
 export const UserDropDown = ({
   userName = 'Nome do usuário',
+  srcImage = '',
   ...rest
 }: UserDropDownProps) => {
   return (
     <UserDropdownWrapper {...rest}>
-      <AvatarDiv />
+      <AvatarDiv src={srcImage} />
       <Dropdown>
         <Text fontSize="ssm">{userName}</Text>
         <CaretDownIcon width={'8.7px'} height={'5.6px'} />
