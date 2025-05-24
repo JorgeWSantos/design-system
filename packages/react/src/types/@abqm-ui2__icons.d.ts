0dcf869dd43a78641e980bae770e7e8ac29104b9
JorgeWS <jorgews.dev@gmail.com>
2025-05-24 20:02:32 -0300

    feat: 🎸 Adicionará types para funcionamento correto da lib de icones

--- packages/react/src/types/@abqm-ui2__icons.d.ts
+++ packages/react/src/types/@abqm-ui2__icons.d.ts
@@ -1,7 +1,10 @@
-// declare module '@abqm-ui2/icons' {
-//   // import { FC, SVGProps } from 'react';
-//   // export const FilterIcon: FC<SVGProps<SVGSVGElement>>;
-//   // export const PlusIcon: FC<SVGProps<SVGSVGElement>>;
-//   // export const PrinterIcon: FC<SVGProps<SVGSVGElement>>;
-//   // Adicione outros ícones exportados aqui
-// }
+declare module '@abqm-ui2/icons' {
+  import { FC, SVGProps } from 'react';
+  export const FilterIcon: FC<SVGProps<SVGSVGElement>>;
+  export const PlusIcon: FC<SVGProps<SVGSVGElement>>;
+  export const PrinterIcon: FC<SVGProps<SVGSVGElement>>;
+  export const CaretDownIcon: FC<SVGProps<SVGSVGElement>>;
+  export const LogoSeqmIcon: FC<SVGProps<SVGSVGElement>>;
+  export const PersonCircleIcon: FC<SVGProps<SVGSVGElement>>;
+  // Adicione outros ícones exportados aqui
+}
