993e1f51302ef21603d844d508ea94ddbb23b429
JorgeWS <jorgews.dev@gmail.com>
2025-05-25 18:47:38 -0300

    feat: 🎸 Adicionará função que verifica o click do usuário fora do componente

---
+++ packages/react/src/hooks/useClickOutside.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+
+export function useClickOutside(
+  ref: React.RefObject<HTMLDivElement | null>,
+  onClickOutside: () => void
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent | TouchEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClickOutside();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, [ref, onClickOutside]);
+}
