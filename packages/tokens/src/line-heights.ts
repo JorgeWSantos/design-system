dada4be58485c2d588362d0c2aa032984db5f86c
JorgeWS <jorgews.dev@gmail.com>
2025-07-11 21:51:36 -0300

    fix: 🐛 Corrigirá lineheights

--- packages/tokens/src/line-heights.ts
+++ packages/tokens/src/line-heights.ts
@@ -1,8 +1,8 @@
 export const lineHeights = {
   initial: '100%',
-  tight: '0.875rem' /* 127.273% */,
+  tight: '120%',
   shorter: '125%',
-  midshort: '2rem' /* 133.333% */,
+  midshort: '133%',
   short: '140%',
   base: '160%',
   tall: '180%',
