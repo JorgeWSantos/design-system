351214f2c8fd17e48cbaeef1bd578c1bd9926517
JorgeWS <jorgews.dev@gmail.com>
2025-07-13 23:30:47 -0300

    feat: 🎸 Melhoria na estilizaçãodo infoCard

--- packages/react/src/components/InfoCard/index.tsx
+++ packages/react/src/components/InfoCard/index.tsx
@@ -1,6 +1,7 @@
 import { ComponentProps, ElementType } from 'react';
 import { InfoCardDiv } from './styles';
 import { Text } from '@components/Text';
+import { colors } from '@abqm-ds/tokens';
 
 export interface InfoCardProps extends ComponentProps<typeof InfoCardDiv> {
   as?: ElementType;
@@ -12,11 +13,20 @@ export const InfoCard = ({ title, subTitle, ...rest }: InfoCardProps) => {
   return (
     <InfoCardDiv {...rest}>
       {title && (
-        <Text fontSize="ssm" fontWeight="semiBold">
+        <Text
+          fontSize="ssm"
+          fontWeight="semiBold"
+          color={colors.white85}
+          lineHeight="short"
+        >
           {title}
         </Text>
       )}
-      {subTitle && <Text fontSize="xxs">{subTitle}</Text>}
+      {subTitle && (
+        <Text fontSize="xxs" color={colors.white50} style={{ marginTop: '-0.3rem' }}>
+          {subTitle}
+        </Text>
+      )}
     </InfoCardDiv>
   );
 };
