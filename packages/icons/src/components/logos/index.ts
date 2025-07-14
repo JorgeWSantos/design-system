1cfc2637774dcb9948a0c7cdce67478134ce24b7
JorgeWS <jorgews.dev@gmail.com>
2025-07-14 07:10:59 -0300

    feat: 🎸 Novo ícone

--- packages/icons/src/components/logos/index.ts
+++ packages/icons/src/components/logos/index.ts
@@ -1,3 +1,4 @@
+export { default as EventSummaryDefaultIcon } from './EventSummaryDefaultIcon';
 export { default as LogoPadraoIcon } from './LogoPadraoIcon';
 export { default as LogoSeqmBlackGreenIcon } from './LogoSeqmBlackGreenIcon';
 export { default as LogoSeqmIcon } from './LogoSeqmIcon';
