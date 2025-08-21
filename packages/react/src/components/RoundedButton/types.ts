fbf1c1c457315cce461d8a9e9cd05d18863a2320
JorgeWS <jorgews.dev@gmail.com>
2025-08-20 21:24:21 -0300

    fix: 🐛 Corrigirá remoção do parâmetro outline-white-25

--- packages/react/src/components/RoundedButton/types.ts
+++ packages/react/src/components/RoundedButton/types.ts
@@ -1,7 +1,12 @@
 import { ComponentProps, ElementType, ReactNode } from 'react';
 import { StyledRoundedButton } from './styles';
 
-export type RoundedButtonVariants = 'primary' | 'outline' | 'secondary' | 'tertiary';
+export type RoundedButtonVariants =
+  | 'primary'
+  | 'outline'
+  | 'secondary'
+  | 'tertiary'
+  | 'outline-white-25';
 
 export interface RoundedButtonProps extends ComponentProps<typeof StyledRoundedButton> {
   as?: ElementType;
