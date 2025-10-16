00a5a0199094103400596b893037769a4004a746
JorgeWS <jorgews.dev@gmail.com>
2025-10-15 22:33:17 -0300

    feat: 🎸 Melhoria no height do ContentDesktop

--- packages/react/src/components/Layout/Desktop/ContentDesktop/index.tsx
+++ packages/react/src/components/Layout/Desktop/ContentDesktop/index.tsx
@@ -2,6 +2,7 @@ import React, { ComponentProps } from 'react';
 import { ContainerDesktop, ContentBox } from './styles';
 import { InforBar } from './InfoBar';
 import { LoadingOverlay } from '@components/LoadingOverlay';
+import { FooterTypes } from '@components/Layout/GlobalContainer/Footer';
 
 interface ContentDektopProps extends ComponentProps<typeof ContainerDesktop> {
   header?: React.ReactNode;
@@ -10,6 +11,7 @@ interface ContentDektopProps extends ComponentProps<typeof ContainerDesktop> {
   count?: number;
   contentBoxStyles?: React.CSSProperties;
   isLoading?: boolean;
+  footerType?: FooterTypes;
 }
 
 export const ContentDektop = ({
@@ -19,18 +21,33 @@ export const ContentDektop = ({
   headerNavigator,
   count,
   isLoading = false,
+  footerType,
   ...rest
 }: ContentDektopProps) => {
   // This is a workaround to set the height of the content based on the screen height.
   // [heightScreen, percentageHeight] = [1300, 0.93];
+
+  const sumByFooterType = () => {
+    switch (footerType) {
+      case 'large':
+        return 0;
+      case 'medium':
+        return 0.03;
+      case 'small':
+        return 0;
+      default:
+        return 0;
+    }
+  };
+
   const heightFactor =
     [
-      [1300, 0.93],
-      [1000, 0.92],
-      [900, 0.9],
-      [800, 0.89],
-      [700, 0.88],
-      [600, 0.87],
+      [1300, 0.93 + sumByFooterType()],
+      [1000, 0.92 + sumByFooterType()],
+      [900, 0.9 + sumByFooterType()],
+      [800, 0.89 + sumByFooterType()],
+      [700, 0.88 + sumByFooterType()],
+      [600, 0.87 + sumByFooterType()],
     ].find(([limit]) => window.innerHeight >= limit)?.[1] ?? 0.84;
 
   return (
