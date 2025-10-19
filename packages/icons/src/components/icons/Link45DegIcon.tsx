ea8ec66f9b0048cede7de935021b5283e391728a
JorgeWS <jorgews.dev@gmail.com>
2025-10-19 09:00:38 -0300

    feat: 🎸 Novos icons

---
+++ packages/icons/src/components/icons/Link45DegIcon.tsx
@@ -0,0 +1,21 @@
+import type { SVGProps } from 'react';
+const Link45DegIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill={props?.fill ? props.fill : '#1E1E1E'}
+      d="m7.072 9.813-2.058 2.058a4.5 4.5 0 0 0 6.365 6.364l2.742-2.743a4.5 4.5 0 0 0-1.242-7.242l-.88.879q-.133.135-.23.299a3 3 0 0 1 1.291 5.005l-2.74 2.742a3.002 3.002 0 1 1-4.245-4.245l1.19-1.188a6 6 0 0 1-.193-1.93z"
+    />
+    <path
+      fill={props?.fill ? props.fill : '#1E1E1E'}
+      d="M9.879 7.008a4.5 4.5 0 0 0 1.242 7.242l1.162-1.164a3 3 0 0 1-1.344-5.019l2.74-2.742a3.002 3.002 0 1 1 4.246 4.245l-1.19 1.188c.168.63.233 1.283.192 1.93l2.058-2.057a4.5 4.5 0 1 0-6.364-6.365z"
+    />
+  </svg>
+);
+export default Link45DegIcon;
