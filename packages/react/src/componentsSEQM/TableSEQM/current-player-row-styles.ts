b53a777f7b202d86e0884ab89889e75f41fe0a80
JorgeWS <jorgews.dev@gmail.com>
2025-11-03 23:25:12 -0300

    feat: 🎸 Melhorias de estilização

--- packages/react/src/componentsSEQM/TableSEQM/current-player-row-styles.ts
+++ packages/react/src/componentsSEQM/TableSEQM/current-player-row-styles.ts
@@ -4,45 +4,40 @@ import { css } from 'styled-components';
 export const currentPlayerRowStyles = css`
   box-sizing: border-box;
 
-  &:nth-child(even) {
-    td:first-child {
-      position: relative;
+  td:first-child {
+    position: relative;
 
-      &::before {
-        content: '';
-        display: block;
-        position: absolute;
-        top: 1.75px;
-        left: 0;
-        width: 2px;
-        height: calc(100% - 4px);
-        background: ${colors.brown400};
-        pointer-events: none;
-        z-index: 1;
-      }
-
-      box-sizing: border-box;
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 1.75px;
+      left: 0;
+      width: 1.5px;
+      height: calc(100% - 4px);
+      background: ${colors.brown400};
+      pointer-events: none;
+      z-index: 1;
     }
+
+    box-sizing: border-box;
   }
 
-  &:nth-child(odd) {
-    td:first-child {
-      position: relative;
-
-      &::before {
-        content: '';
-        display: block;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 2px;
-        height: 100%;
-        background: ${colors.brown400};
-        pointer-events: none;
-        z-index: 1;
-      }
-
-      box-sizing: border-box;
+  td:last-child {
+    position: relative;
+    &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 1.75px;
+      right: 0;
+      width: 2px;
+      height: calc(100% - 4px);
+      background: ${colors.brown400};
+      pointer-events: none;
+      z-index: 1;
     }
+
+    box-sizing: border-box;
   }
 `;
